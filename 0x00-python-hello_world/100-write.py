b7262e1373f883042d22735a4a497c91690bbbcc
tommydebisi <tommydebisi@gmail.com>
2022-05-30 22:01:06 +0100

    :art: task100 update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-err = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
-sys.exit(err)
+err = 'and that piece of art is useful - Dora Korpar, 2015-10-19\n'
+sys.stderr.write(err)
+sys.exit(1)
