7e515b3ef9ddb38859307475a687c6172af4e30e
tommydebisi <tommydebisi@gmail.com>
2022-05-30 18:25:18 +0100

    :penguin: task10 added, :art: task8 update4

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_cycle - checks a list for a loop
+ *
+ * @list: list to be checked
+ * Return: 1 if there is loop or 0 if otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+
+	tortoise = hare = list;
+	tortoise = tortoise->next;
+	hare = hare->next->next;
+	while (tortoise && hare && hare->next)
+	{
+		if (tortoise == hare)
+			return (1); /* Loop found */
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	/* No loop/cycle found */
+	return (0);
+}
