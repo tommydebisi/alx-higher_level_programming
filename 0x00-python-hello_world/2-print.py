80b9db38dc351694639bf920b463e3cfac4135c3
tommydebisi <tommydebisi@gmail.com>
2022-05-30 14:54:38 +0100

    :penguin: mandatory tasks 0-9 added

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    print("\"Programming is like building a multilingual puzzle")
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+with open("zenP.txt") as f:
+    for wo in f:
+        print(wo, end="")
+print()
