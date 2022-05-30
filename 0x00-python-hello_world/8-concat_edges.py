6d28bd7e12ba537975469014230870baba824f9b
tommydebisi <tommydebisi@gmail.com>
2022-05-30 17:44:40 +0100

    [fix] - task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
+
 print(str[str.find('object'):str.find('that')] + str[str.find('with'): str.find("very")] + str[:6])
