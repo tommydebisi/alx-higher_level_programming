a2bd811cf6d806cd0c056f60f80406917af8b564
tommydebisi <tommydebisi@gmail.com>
2022-06-02 15:16:08 +0100

    :white_check_mark: mandatory task complete

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+
     number_of_args = len(argv) - 1
 
     if number_of_args == 0:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+
     argument_length = len(argv) - 1
     if argument_length == 0:
         print(0)
