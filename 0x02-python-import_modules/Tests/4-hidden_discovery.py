6a3905c3ab0b3c881796b5c522e73908c4a7f4d2
tommydebisi <tommydebisi@gmail.com>
2022-06-02 15:23:21 +0100

    :recycle: changed to normal loop

--- 0x02-python-import_modules/Tests/4-hidden_discovery.py
+++ 0x02-python-import_modules/Tests/4-hidden_discovery.py
@@ -2,5 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = [word for word in dir(hidden_4) if not (word.startswith('__'))]
-    print('\n'.join(names))
+    for word in dir(hidden_4):
+        if not (word.startswith('__')):
+            print(word)
