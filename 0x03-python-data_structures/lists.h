b25ea9d255a85be9419b24395f7375c0be42edb1
tommydebisi <tommydebisi@gmail.com>
2022-06-05 22:42:31 +0100

    :art: improved task13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -22,5 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int list_len(const listint_t *head);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LISTS_H */
