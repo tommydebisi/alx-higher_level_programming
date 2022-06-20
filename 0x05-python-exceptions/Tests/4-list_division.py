60cd45c069afa94c33ba5c6f1ab572bc18c6f5ce
tommydebisi <tommydebisi@gmail.com>
2022-06-20 12:19:11 +0100

    :construction: tests added

---
+++ 0x05-python-exceptions/Tests/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    divides element by element 2 lists.
+
+    Args:
+        my_list_1: first list
+        my_list_2: second list
+        list_length: length of the list
+    """
+    new_list = list()
+    for iter in range(list_length):
+        try:
+            new_list.append(my_list_1[iter] / my_list_2[iter])
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+    return new_list
---
+++ 0x05-python-exceptions/Tests/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    raises a name exception with a message
+
+    Args:
+        message: message to be printed
+    """
+    raise NameError(message)
