1df7c9f76b9424dd491f3036f872e47f52bf56e8
tommydebisi <tommydebisi@gmail.com>
2022-06-20 17:15:35 +0100

    :construction: advanced tasks added

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    """
+    Function that prints an integer
+
+    Args:
+        value:  any type(integers, strings, etc)
+
+    Return:
+        True if value has been correctly printed
+        else false
+    """
+    from sys import stderr
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print("Exception: {}".format(err), file=stderr)
+        return False
