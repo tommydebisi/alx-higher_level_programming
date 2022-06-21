02f2c53884114b5e1ae7fd69bef3b47fb8b41f47
tommydebisi <tommydebisi@gmail.com>
2022-06-21 20:43:11 +0100

    :construction: tested for type tuple

--- 0x06-python-classes/Tests/6-square.py
+++ 0x06-python-classes/Tests/6-square.py
@@ -37,7 +37,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
