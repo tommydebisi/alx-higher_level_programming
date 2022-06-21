73a5c7ddc4d2a69d5b861bedf038dd658ff6b5f4
tommydebisi <tommydebisi@gmail.com>
2022-06-21 20:56:33 +0100

    :seedling: updated tuple test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,10 +37,14 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or (value[0] < 0 or value[1] < 0):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = value
+            self.__position = value     # tuple contains 2 positive integers
 
     def area(self):
         """ Computes area of a square """
