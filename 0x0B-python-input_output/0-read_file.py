5c51ed5e10bb743b581da4b629f1a81d122af51f
tommydebisi <tommydebisi@gmail.com>
2022-07-05 23:05:37 +0100

    :seedling: end added in print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,4 @@ def read_file(filename=""):
             filename: name of file to be read
     """
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end='')
