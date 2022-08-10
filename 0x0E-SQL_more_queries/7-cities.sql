2293031504c76f7c3a95cc5bf3e1ad1250dcb843
tommydebisi <tommydebisi@gmail.com>
2022-08-10 22:01:23 +0100

    :seedling: on delete added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -12,6 +12,6 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (cities.id),
-    FOREIGN KEY (state_id) REFERENCES states(states.id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE
 );
