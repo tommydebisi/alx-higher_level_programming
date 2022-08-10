94616f1722b781844baea3a0d93125386642c42e
tommydebisi <tommydebisi@gmail.com>
2022-08-10 23:10:27 +0100

    :seedling: added to the tables

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,14 @@
 -- Modifying a column by adding constraint
 ALTER TABLE tv_show_genres MODIFY genre_id INT DEFAULT NULL;
 
+-- Inserting show titles into tv_shows
+INSERT INTO tv_shows(title) VALUES('Better Call Saul');
+INSERT INTO tv_shows(title) VALUES('Homeland');
+
+-- Inserting show id into tv_show_genres
+INSERT INTO tv_show_genres(show_id) VALUES(11);
+INSERT INTO tv_show_genres(show_id) VALUES(12);
+
 -- using join to dsplay data and order based on title and genre id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
