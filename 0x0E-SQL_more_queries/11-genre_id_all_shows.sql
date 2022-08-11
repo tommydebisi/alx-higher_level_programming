40820b1235564a6f01826a0883ed80783cf32346
tommydebisi <tommydebisi@gmail.com>
2022-08-11 03:42:29 +0100

    :art: use of left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,19 +1,8 @@
 -- This script lists all shows contained in the database hbtn_0d_tvshows.
 
--- Modifying a column by adding constraint
-ALTER TABLE tv_show_genres MODIFY tv_show_genres.genre_id INT DEFAULT NULL;
-
--- Inserting show titles into tv_shows
-INSERT INTO tv_shows(title) VALUES('Better Call Saul');
-INSERT INTO tv_shows(title) VALUES('Homeland');
-
--- Inserting show id into tv_show_genres
-INSERT INTO tv_show_genres(show_id) VALUES(11);
-INSERT INTO tv_show_genres(show_id) VALUES(12);
-
 -- using join to dsplay data and order based on title and genre id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-JOIN tv_shows
+FROM tv_shows
+LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
