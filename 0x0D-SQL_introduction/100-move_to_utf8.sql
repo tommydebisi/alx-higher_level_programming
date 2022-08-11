e0dabcfa05b6cd5f1690af866b3a29753be48996
tommydebisi <tommydebisi@gmail.com>
2022-08-11 13:57:11 +0100

    :seedling: alter if exists

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 -- UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
 
 -- ALTER the database with modified constraints
-ALTER DATABASE hbtn_0c_0
+ALTER DATABASE IF EXISTS hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
@@ -10,12 +10,12 @@ COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 
 -- ALTER the table with modified constraints
-ALTER TABLE first_table
+ALTER TABLE IF EXISTS first_table
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- ALTER the column with modified constraints
-ALTER TABLE first_table
+ALTER TABLE IF EXISTS first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
