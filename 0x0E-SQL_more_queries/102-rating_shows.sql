71b104d37d89dfdf388bb1e303422ddb6263a44d
tommydebisi <tommydebisi@gmail.com>
2022-08-11 16:48:28 +0100

    :bug: fix the join bug

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,9 +3,7 @@
 -- Use of inner join
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-FULL OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-FULL OUTER JOIN tv_show_ratings
-ON tv_show_ratings.show_id = tv_show_genres.show_id
+JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
