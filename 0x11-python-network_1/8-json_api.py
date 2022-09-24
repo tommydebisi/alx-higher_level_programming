a1ef079ebcac9af9619afe146e544cddae6a0129
tommydebisi <tommydebisi@gmail.com>
2022-09-24 05:07:25 +0100

    :seedling: try and except used

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,9 +11,13 @@ if __name__ == "__main__":
         post_dic = {'q': ""}
     else:
         post_dic = {'q': argv[1]}
-    with requests.post("http://0.0.0.0:5000/search_user", data=post_dic) as r:
-        dic_val = r.json()
-        if dic_val:
-            print("[{}] {}".format(dic_val['id'], dic_val['name']))
-        else:
-            print("No result")
+
+    try:
+        with requests.post("http://0.0.0.0:5000/search_user", data=post_dic) as r:
+            dic_val = r.json()
+            if dic_val:
+                print("[{}] {}".format(dic_val['id'], dic_val['name']))
+            else:
+                print("No result")
+    except Exception:
+        print("Not a valid JSON")
