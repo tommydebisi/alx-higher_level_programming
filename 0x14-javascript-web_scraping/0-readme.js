caa661c5f442b7e3e3c1c55a93af99ad31c3f5bf
tommydebisi <tommydebisi@gmail.com>
2022-10-18 20:51:50 +0100

    :construction: task0, reading from a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This script reads and prints the content of a file.
+const fs = require('fs');
+
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
