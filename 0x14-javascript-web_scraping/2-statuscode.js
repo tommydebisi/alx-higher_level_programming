89daf2010b9e83284a577da9a5353f1b6fa877ef
tommydebisi <tommydebisi@gmail.com>
2022-10-18 21:11:39 +0100

    :construction: task 1, 2 added

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// This script display the status code of a GET request.
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, (err, res, body) => {
+  console.log(res.statusCode);
+});
